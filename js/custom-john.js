63b4757438aba22fc35ad9c86709a3cb45efa6da
John Lito Bardinas <bardinasjohnlito@gmail.com>
2020-02-16 15:45:07 +0800

    Cleaning the service section and cleaning the code for the Profile constructor function

--- js/custom-john.js
+++ js/custom-john.js
@@ -1,20 +1,80 @@
-/* this must be included!! */
-const johnObj = {
-	age: 		  (dt.getFullYear()-1996),
-	fname: 		  'John Lito',
-	lname: 		  'Bardinas',
-	title: 		  'Full Stack Developer',
-	exp_nonprof:  '4 Years',
-	exp_prof:  	  '2 Years',
-	from:  		  'Makati, Philippines',
-	availability: 'Weekends only'
+/**
+ * Ow!! you came here..
+ */
+
+function Profile() {
+
+	// Instance Member.
+	var fullName = 'John Lito Bardinas';
+	var firstName = 'John Lito';
+	var lastName = 'Bardinas';
+	var dateOfBith = new Date('December 15, 1996');
+	var nonProYear = new Date('May 1, 2016');
+	var proYear = new Date('August 18, 2017');
+	var title = 'Full Stack ';
+	var fromLocation = 'Philippines';
+	var availability = 'Weekends Only';
+
+	/**
+	 * Return the computed date
+	 * @return Computed Age.
+	 */
+	this.computeAge = function () {
+		var diff_ms = Date.now() - dateOfBith.getTime();
+		var age_dt = new Date(diff_ms);
+		return Math.abs(age_dt.getUTCFullYear() - 1970);
+	}
+
+	/**
+	 * Return the computed Working Years.
+	 * @param {boolean} isProfessionalYear Determine if Professional Year.
+	 * @return COmputed Year.
+	 */
+	this.computeWorkingYear = function (isProfessionalYear) {
+		if (isProfessionalYear) {
+			return Math.abs(new Date().getFullYear() - proYear.getFullYear());
+		}
+
+		return Math.abs(new Date().getFullYear() - nonProYear.getFullYear());
+
+	}
+
+	// Define readonly properties.
+	Object.defineProperties(this, {
+		fullName: {
+			value: fullName,
+			writable: false
+		},
+		'firstName': {
+			value: firstName,
+			writtable: false
+		},
+		'lastName': {
+			value: lastName,
+			writtable: false
+		},
+		'title': {
+			value: title,
+			writable: false
+		},
+		'fromLocation': {
+			value: fromLocation,
+			writable: false
+		},
+		'availability': {
+			value: availability,
+			writable: false
+		}
+	});
 
 }
 
-document.getElementById('age').textContent = johnObj.age;
-document.getElementById('from').textContent = johnObj.from;
-document.getElementById('exp_prof').textContent = johnObj.exp_prof;
-document.getElementById('exp_nonprof').textContent = johnObj.exp_nonprof;
-document.getElementById('availability').textContent = johnObj.availability;
-
-
+$(function () {
+	var johnLito = new Profile();
+	$('#full-name').text(johnLito.fullName);
+	$('#age').text(johnLito.computeAge());
+	$('#title').text(johnLito.title);
+	$('#from').text(johnLito.fromLocation);
+	$('#exp_prof').text(johnLito.computeWorkingYear(true));
+	$('#exp_nonprof').text(johnLito.computeWorkingYear());
+});
